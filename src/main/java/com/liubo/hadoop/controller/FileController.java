7064b1fe4649dc06182d5032af89177ed456e247
Lonseajim <lonseajim@163.com>
2019-05-25 08:06:52 +0800

    bo:使用postgresql

---
+++ src/main/java/com/liubo/hadoop/controller/FileController.java
@@ -0,0 +1,10 @@
+package com.liubo.hadoop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lonse
+ */
+@RestController
+public class FileController {
+}
--- src/main/java/com/liubo/hadoop/controller/PageController.java
+++ src/main/java/com/liubo/hadoop/controller/PageController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author lonse
+ */
 @Controller
 public class PageController {
 
     @RequestMapping("/")
     public String hello(){
-        return "hello";
+        return "file_list";
     }
 
     @RequestMapping("{page}")
